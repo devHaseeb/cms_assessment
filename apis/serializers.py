70b8e30be50e795b253551577154282b531685ce
Haseeb Raza <haseebraza@revolveai.com>
2023-02-02 14:02:10 +0500

    fixes on filters

--- apis/serializers.py
+++ apis/serializers.py
@@ -4,41 +4,26 @@ import random
 import string
 from auditlog.models import LogEntry
 
+
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = '__all__'
 
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id', 'uuid', 'first_name', 'last_name', 'nationality', 'age', 'location', 'student_id', 'school','created_at','updated_at']
-    
+        fields = ['id', 'uuid', 'first_name', 'last_name', 'nationality',
+                  'age', 'location', 'student_id', 'school', 'created_at', 'updated_at']
+
     def validate(self, data):
         school = data.get('school', None)
         if school and school.max_students <= school.student_set.count():
             raise serializers.ValidationError('The selected school is full')
         return data
-    
-    def get_queryset(self):
-        queryset = Student.objects.all()
-        age = self.request.query_params.get('age', None)
-        nationality = self.request.query_params.get('nationality', None)
-        location = self.request.query_params.get('location', None)
-        student_id = self.request.query_params.get('student_id', None)
-
-        if age is not None:
-            queryset = queryset.filter(age__gt=age)
-        if nationality is not None:
-            queryset = queryset.filter(nationality__iexact=nationality)
-        if location is not None:
-            queryset = queryset.filter(location__iexact=location)
-        if student_id is not None:
-            queryset = queryset.filter(student_id__iexact=student_id)
-        return queryset
 
 class LogEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = LogEntry
         fields = '__all__'
-    
--- apis/views.py
+++ apis/views.py
@@ -16,7 +16,28 @@ class SchoolViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+    def get_queryset(self):
+        age = self.request.query_params.get('age', None)
+        nationality = self.request.query_params.get('nationality', None)
+        location = self.request.query_params.get('location', None)
+        student_id = self.request.query_params.get('student_id', None)
+        
+        queryset = self.queryset
+        if age is not None:
+            queryset = queryset.filter(age__gt=age)
+        if nationality is not None:
+            queryset = queryset.filter(nationality__iexact=nationality)
+        if location is not None:
+            queryset = queryset.filter(location__iexact=location)
+        if student_id is not None:
+            queryset = queryset.filter(student_id__iexact=student_id)
+        return queryset
 
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+    #     context['request'] = self.request
+    #     return context
 
 class LogEntryViewSet(viewsets.ModelViewSet):
     queryset = LogEntry.objects.all()
